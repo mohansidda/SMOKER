02bf7688b0d237bbe6f6cd4581eaac5b6bb31d72
Stanislav Knot <sknot@redhat.com>
2019-04-18 13:58:34 +0200

    update configs

--- expoDemoUser1/getAuthAndStart.sh
+++ expoDemoUser1/getAuthAndStart.sh
@@ -15,6 +15,8 @@ rm user.crt user.key ca.crt
 bootstrap=`oc get service my-cluster-kafka-external-bootstrap -o=jsonpath='{.status.loadBalancer.ingress[0].ip}{"\n"}'`
 bootstrap="${bootstrap}:9094"
 
+echo $bootstrap
+
 export SMOKER_BOOTSTRAP_SERVERS=${bootstrap}
 echo $SMOKER_BOOTSTRAP_SERVERS
 export SMOKER_TRUSTSTORE_PASSWORD="123456"
